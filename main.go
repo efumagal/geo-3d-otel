558efb7229073f5c1198aaae24d8e1c9c035964e
efumagal <77152760+efumagal@users.noreply.github.com>
2023-08-08 12:11:26 +0100

    Update main.go

--- main.go
+++ main.go
@@ -79,9 +79,9 @@ func main() {
 		return c.JSON(map[string]any{"distance": distance})
 	})
 
-	app. Post("/action/cpu-load", func(c *fiber.Ctx) error {
+	app.Post("/action/cpu-load", func(c *fiber.Ctx) error {
 		// Create a child span
-		_, childSpan := tracer.Start(c.UserContext(), "distance_computation")
+		ctx, childSpan := tracer.Start(c.UserContext(), "distance_computation")
 
 		for i := 0; i < 1_000; i++ {
 			start := geo.NewCoord3d(randFloat(-90, 90), randFloat(-180, 180), randFloat(0, 10000))
